6b42a88c4608d4bc31956ac2b03844dc6b0393c7
Philip Chauvet <pchauvet@gmail.com>
2020-04-05 03:14:33 -0400

    update

--- src/main/java/com/aikiinc/coronavirus/CornaVirusController.java
+++ src/main/java/com/aikiinc/coronavirus/CornaVirusController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@CrossOrigin(origins = "https://pure-shore-41784.herokuapp.com", maxAge = 3600)
+//@CrossOrigin(origins = "https://pure-shore-41784.herokuapp.com", maxAge = 3600)
 //@CrossOrigin(origins = "http://localhost:9000", maxAge = 3600)
 //@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
--- src/main/java/com/aikiinc/coronavirus/CoronaVirusApp.java
+++ src/main/java/com/aikiinc/coronavirus/CoronaVirusApp.java
@@ -2,6 +2,9 @@ package com.aikiinc.coronavirus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CoronaVirusApp {
@@ -10,4 +13,15 @@ public class CoronaVirusApp {
 		SpringApplication.run(CoronaVirusApp.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/virus/corona").allowedOrigins("http://localhost:9000");
+				registry.addMapping("/api/virus/corona").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/api/virus/corona").allowedOrigins("https://pure-shore-41784.herokuapp.com");
+			}
+		};
+	}
 }
